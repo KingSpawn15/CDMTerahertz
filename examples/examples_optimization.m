f6a74d819c30dbaebff1d7f782def80f5159f002
kingspawn15 <kaizad.rustomji@gmail.com>
2021-12-28 21:25:32 +0200

    disp

--- examples/examples_optimization.m
+++ examples/examples_optimization.m
@@ -2,4 +2,5 @@ fun = @(x) optimization.cost_function(x);
 x0 = [0.05, 0.1];
 lb = [-0.1, 0];
 ub = [0.2, 0.15];
-[x,resnorm,residual,exitflag,output] = lsqnonlin(fun,x0,lb,ub);
+options = optimoptions('lsqnonlin','Display','iter');
+[x,resnorm,residual,exitflag,output] = lsqnonlin(fun,x0,lb,ub,options);
