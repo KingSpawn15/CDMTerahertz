e85309c18de6387c2458a0b1a277e2a58a22a2e3
kingspawn15 <kaizad.rustomji@gmail.com>
2022-03-31 16:01:48 +0300

    to go

--- examples/single_spectrum.m
+++ examples/single_spectrum.m
@@ -1,9 +1,14 @@
 clearvars;
-[~ , ~ , ~] = mkdir('results/combination/exhaustive/');
+
+for energy_gain_factor = 1
+%     energy_gain_factor = 1;
+dir = 'results/power_checks_or/';
+[~ , ~ , ~] = mkdir(dir);
 load('saved_matrices/v_struct_5deg.mat');
-factor_rect = 0.5;
+factor_rect = 0.3;
 factor_pd = 0;
-delay = -15;
+delay_pd = -15;
+delay_or = 6;
 
 [laser_parameters,discretization_params, utem_parameters,...
     numerical_parameters] = default_parameters_2();
@@ -16,8 +21,12 @@ utem_parameters.electron_total_time_fs = 350;
 utem_parameters.electron_time_coherent_fwhm_fs = 20;
 utem_parameters.electron_theta = -6.35*pi/180;
 utem_parameters.electron_velocity_c = 0.7;
-% numerical_parameters.subsampling_factor = 20;
+numerical_parameters.subsampling_factor = 20;
 
+pulse_width_fs = 50;
+% energy_gain_factor = 50;
+laser_parameters.pulse_energy_gain_factor = 0.014 * energy_gain_factor;
+laser_parameters.laser_pulse_time_fwhm = pulse_width_fs*1e-15;
 laser = Laser(laser_parameters);
 
 discretization = Discretization(discretization_params);
@@ -42,9 +51,9 @@ for interaction_gain_factor_rectification = factor_rect
         
         plot_ind = 1;
         close all;
-        figure = tiledlayout(1,2,'Padding', 'none', 'TileSpacing', 'compact');
+        figure = tiledlayout(1,1,'Padding', 'none', 'TileSpacing', 'compact');
         kk = 1;
-        for theta_pol_degree = [1 3]*45
+        for theta_pol_degree = 90
             
             
             laser.theta_pol =  theta_pol_degree.*(pi/180);
@@ -52,15 +61,34 @@ for interaction_gain_factor_rectification = factor_rect
             
             eels = EELS(eels_parameters);
             
+            loss_spectrum_parameters.interaction_gain_factor_rectification = ...
+                1;
+            loss_spectrum_parameters.interaction_gain_factor_photodember =...
+                0 ;
+            loss_spectrum_parameters.method = 'rectification';
+            v_or = eels.interaction_v(loss_spectrum_parameters);
+            
+            loss_spectrum_parameters.interaction_gain_factor_rectification = ...
+                0;
+            loss_spectrum_parameters.interaction_gain_factor_photodember =...
+                1 ;
+            loss_spectrum_parameters.method = 'photodember';
+            v_pd = eels.interaction_v(loss_spectrum_parameters);
+            
+            
             
             loss_spectrum_parameters.interaction_gain_factor_rectification = ...
                 interaction_gain_factor_rectification;
             loss_spectrum_parameters.interaction_gain_factor_photodember =...
                 interaction_gain_factor_photodember ;
-            loss_spectrum_parameters.method = 'rectification';
-            %             loss_spectrum_parameters.interact_v = interaction_gain_factor_rectification * ...
-            %                 circshift(v_struct.(strcat('angle_',num2str(theta_pol_degree))),[0  0]) + ...
-            %                 interaction_gain_factor_photodember * circshift(v_struct.(strcat('photodember')),[delay 0]);
+            loss_spectrum_parameters.method = 'combination';
+%             loss_spectrum_parameters.interact_v = interaction_gain_factor_rectification * ...
+%                 circshift(v_struct.(strcat('angle_',num2str(theta_pol_degree))),[0  0]) + ...
+%                 interaction_gain_factor_photodember * circshift(v_struct.(strcat('photodember')),[delay 0]);
+            loss_spectrum_parameters.interact_v = interaction_gain_factor_rectification * ...
+                circshift(v_or,[delay_or  0]) + ...
+                interaction_gain_factor_photodember * circshift(v_pd,[delay_pd 0]);
+            
             [psi_sub , psi_incoherent] = eels.energy_loss_spectrum(loss_spectrum_parameters);
             nexttile;
             imagesc(e_w,t_w, psi_incoherent);
@@ -70,24 +98,27 @@ for interaction_gain_factor_rectification = factor_rect
             %                 xlim([-3,3]);
             %                 ylim([-0.5,1]);
             
-            if plot_ind == 9 || plot_ind == 18
+            %                 if plot_ind == 9 || plot_ind == 18
+            if 1==1
                 colorbar;
             end
             colormap jet
             axis square
             ax = gca;
-            ax.FontSize = 10;
+            ax.FontSize = 18;
             ax.LineWidth = 1;
-            if (true)
+            %             if (plot_ind == 1 || plot_ind == 10)
+            if 1==1
                 ax.YTick = -1:0.5:1.5;
-                ylabel('\Deltat [ps]','Color',[0.3 0.3 0.3],'FontSize',16);
+                ylabel('\Deltat [ps]','Color',[0.3 0.3 0.3],'FontSize',18);
             else
                 ax.YTick = [];
             end
             
-            if (true)
+            %             if (plot_ind >= 10)
+            if 1==1
                 ax.XTick = -4:2:4;
-                xlabel('Energy (eV)','Color',[0.3 0.3 0.3],'FontSize',16);
+                xlabel('Energy (eV)','Color',[0.3 0.3 0.3],'FontSize',18);
             else
                 ax.XTick = [];
             end
@@ -95,20 +126,19 @@ for interaction_gain_factor_rectification = factor_rect
             box on
             plot_ind = plot_ind + 1;
             
-            str = ['results/combination/delay/','combination_',...
-                'angle=',num2str(theta_pol_degree),...
-                'pd_gain=',num2str(interaction_gain_factor_photodember),...
-                'or_gain=',num2str(interaction_gain_factor_rectification)
-                ];
-            exportgraphics(gcf, strcat(str,'_none_yes_y0.png'),'resolution' , 400);
-            
         end
-        %         set(gcf,'Position',[100,100,1600,400]);
-        
+        %             set(gcf,'Position',[100,100,1600,400]);
+        str = [dir,'combination_',...
+            'energy_gain=',num2str(energy_gain_factor),...
+            'angle=',num2str(theta_pol_degree),...
+            'pd_gain=',num2str(interaction_gain_factor_photodember),...
+            'or_gain=',num2str(interaction_gain_factor_rectification)
+            ];
+        exportgraphics(gcf, strcat(str,'.png'),'resolution' , 400);
     end
 end
 
 
 
 
-
+end
